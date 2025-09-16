d6c95fa9addda89d224a0fa372abca227c34dd81
ajjones-5 <anthonjones@claflin.edu>
2025-09-16 12:36:29 -0400

    Add files via upload

---
+++ Microarray-Analysis.R
@@ -0,0 +1,10 @@
+#Author: Anthony, Date: 09/16/2025, Purpose: Microarray analysis
+
+#Install package affy
+#Load package affy
+
+library(affy)
+
+#Reading sample microarray data
+
+setwd("/Users/vajones/Desktop")
