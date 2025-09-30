60b5e2af8f42430a33bc64cc5a5b8aa45f4f11cd
ajjones-5 <anthonjones@claflin.edu>
2025-09-30 12:34:48 -0400

    Add files via upload

--- Microarray-Analysis.R
+++ Microarray-Analysis.R
@@ -6,5 +6,12 @@
 library(affy)
 
 #Reading sample microarray data
+setwd("/Users/vajones/Desktop/data2")
 
-setwd("/Users/vajones/Desktop")
+#Read raw microarray data 
+library(oligo)
+cel_files <- list.celfiles("/Users/vajones/Desktop/data2", full.names = TRUE)
+rawData <- read.celfiles(cel_files)
+
+#Boxplot raw data
+boxplot(exprs(rawData))
