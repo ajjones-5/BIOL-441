7b7f5a4beb392ff7ee528f4511c633cbbd98916a
ajjones-5 <anthonjones@claflin.edu>
2025-11-04 12:09:54 -0500

    Add files via upload

--- Microarray-Analysis.R
+++ Microarray-Analysis.R
@@ -15,3 +15,25 @@ rawData <- read.celfiles(cel_files)
 
 #Boxplot raw data
 boxplot(exprs(rawData))
+
+#Perform Normalization
+normdata <- rma(rawData)
+
+#Plot the normalized data
+boxplot(normdata)
+
+#Assume first 2 CEL files are cancer type and next 2 CEL files are normal/control
+
+data2 <- exprs(normdata)
+Treatment <- data2[,c(1:2)]
+Control <- data2[,c(3:4)]
+
+
+#Take average for treatments and controls
+Treatment2 <- rowMeans(Treatment)
+Control2 <- rowMeans(Control)  
+
+#Fold change analysis to compare treatment with control
+fold_change <- Treatment2-Control2
+
+>2 = gene up-regulated and <-2 gene down-regulated
